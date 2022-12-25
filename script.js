95f2ae5f0c87a8fe48e2018dda68395e854a9309
Gabriela Nomberto Ramos <U202113876@upc.edu.pe>
2022-12-24 22:59:33 -0500

    regalo de navidad

---
+++ script.js
@@ -0,0 +1,8 @@
+var sound = new Audio();
+sound.src="soundtracks/music.mp3"
+
+var sound1 = new Audio();
+sound1.src="soundtracks/boton.mp3"
+
+var sound2 = new Audio();
+sound2.src="soundtracks/music1.mp3"
